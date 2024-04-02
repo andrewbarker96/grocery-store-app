d9a69ed2afb3eeab2af444ba884119f5126010cd
Andrew Barker <andrewbarker96@gmail.com>
2024-04-02 15:46:00 -0500

    DB Setup

--- src/server/database_client.d.ts
+++ src/server/database_client.d.ts
@@ -1,10 +1,14 @@
-
 declare module 'database_client' {
   export class Supabase {
     getItems(): Promise<any>;
     addItem(name: string, price: number, image: string): Promise<void>;
     deleteItem(id: number): Promise<void>;
-    updateItem(id: number, name: string, price: number, image: string): Promise<void>;
+    updateItem(
+      id: number,
+      name: string,
+      price: number,
+      image: string
+    ): Promise<void>;
     addUser(email: string, password: string): Promise<void>;
     changePassword(email: string, password: string): Promise<void>;
   }
