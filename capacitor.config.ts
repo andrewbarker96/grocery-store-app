65df2fc7655f316593490ddf88f15f63756243d6
Andrew Barker <andrewbarker96@gmail.com>
2024-04-08 14:57:39 -0500

    package changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'GroceryStoreApp',
-  webDir: 'www/browser',
+  webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
