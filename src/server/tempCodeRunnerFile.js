b3df97b2dbeb89d4ffb22d703b81f5f4474f6cb3
Andrew Barker <andrewbarker96@gmail.com>
2024-04-08 13:38:01 -0500

    Plugins added

--- src/server/tempCodeRunnerFile.js
+++ src/server/tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-environment
+        await supabase.addItem('Red Grapes (3lb)', 4.99, 'https://via.placeholder.com/150');
+        await supabase.deleteItem(5);
+        await supabase.addItem('Green Grapes (3lb)', 4.99, 'https://via.placeholder.com/150');
+        await supabase.addUser('johndoe@live.maryville.edu', 'password');
+        await supabase.changePassword('johndoe@live.maryville.edu', 'newpassword');
