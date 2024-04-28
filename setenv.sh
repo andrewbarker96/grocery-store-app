622aead714e04572b1b0b111ebbc5b89656d96e5
Andrew Barker <andrewbarker96@gmail.com>
2024-04-28 15:17:40 -0500

    setenv.sh change

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-cat <<EOF >src/environments/environment.ts
+cat <<EOF > src/environments/environment.ts
 export const environment = {
   production: false,
+  dbHost: '$DB_HOST',
+  dbName: '$DB_NAME',
+  dbUser: '$DB_USER',
+  dbPassword: '$DB_PASSWORD',
+  supabaseURI: '$SUPABASE_URI',
+  supabaseUrl: '$SUPABASE_URL',
+  supabaseKey: '$SUPABASE_KEY',
+  supabaseSecretKey: '$SUPABASE_SECRET_KEY',
+};
+EOF
+
+cat <<EOF > src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  dbHost: '$DB_HOST',
+  dbName: '$DB_NAME',
+  dbUser: '$DB_USER',
+  dbPassword: '$DB_PASSWORD',
+  supabaseURI: '$SUPABASE_URI',
+  supabaseUrl: '$SUPABASE_URL',
+  supabaseKey: '$SUPABASE_KEY',
+  supabaseSecretKey: '$SUPABASE_SECRET_KEY',
+};
+EOF
+
+cat <<EOF > src/environments/environment.js
+export const environment = {
+  production: true,
   dbHost: '$DB_HOST',
   dbName: '$DB_NAME',
   dbUser: '$DB_USER',
