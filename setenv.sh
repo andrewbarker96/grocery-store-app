d6983d28a16b8666b6e20e3b9ec1bb47a771d764
Andrew Barker <andrewbarker96@gmail.com>
2024-04-28 14:58:43 -0500

    environment workaround

---
+++ setenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat <<EOF >src/environments/environment.ts
+export const environment = {
+  production: false,
+  dbHost: '$DB_HOST',
+  dbName: '$DB_NAME',
+  dbUser: '$DB_USER',
+  dbPassword: '$DB_PASSWORD',
+  supabaseURI: '$SUPABASE_URI',
+  supabaseUrl: '$SUPABASE_URL',
+  supabaseKey: '$SUPABASE_KEY',
+  supabaseSecretKey: '$SUPABASE_SECRET_KEY',
+};
+EOF
