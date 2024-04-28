6d664b7bba1b6518e62a8d224fd1643749ee87ca
Andrew Barker <andrewbarker96@gmail.com>
2024-04-28 15:31:12 -0500

    -

--- setenv.sh
+++ setenv.sh
@@ -13,31 +13,3 @@ export const environment = {
   supabaseSecretKey: '$SUPABASE_SECRET_KEY',
 };
 EOF
-
-cat <<EOF > src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  dbHost: '$DB_HOST',
-  dbName: '$DB_NAME',
-  dbUser: '$DB_USER',
-  dbPassword: '$DB_PASSWORD',
-  supabaseURI: '$SUPABASE_URI',
-  supabaseUrl: '$SUPABASE_URL',
-  supabaseKey: '$SUPABASE_KEY',
-  supabaseSecretKey: '$SUPABASE_SECRET_KEY',
-};
-EOF
-
-cat <<EOF > src/environments/environment.js
-export const environment = {
-  production: true,
-  dbHost: '$DB_HOST',
-  dbName: '$DB_NAME',
-  dbUser: '$DB_USER',
-  dbPassword: '$DB_PASSWORD',
-  supabaseURI: '$SUPABASE_URI',
-  supabaseUrl: '$SUPABASE_URL',
-  supabaseKey: '$SUPABASE_KEY',
-  supabaseSecretKey: '$SUPABASE_SECRET_KEY',
-};
-EOF
